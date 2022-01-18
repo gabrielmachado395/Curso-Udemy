b3b1c8f035a15cb4d15c15802952944282412148
Gabriel <gabrielmachado.emp@gmail.com>
2022-01-18 20:42:44 -0300

    Conteúdo do Curso da Udemy

--- Python/Aula-52/main.py
+++ Python/Aula-52/main.py
@@ -1,9 +1,14 @@
 from pessoas import Pessoa
 
 p1 = Pessoa('Noah', 18)
+p2 = Pessoa('Lucas', 25)
 # p1 != p2
 
 p1.comer('goiaba')
 p1.parar_comer()
 p1.falar('POO')
 p1.comer('maçã')
+
+p2.falar('Dark')
+p2.parar_falar()
+p2.comer('Banana')
