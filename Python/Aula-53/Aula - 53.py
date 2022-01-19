93cdddec8b824f4e994f1174082afcd64b04c00b
Gabriel <gabrielmachado.emp@gmail.com>
2022-01-18 21:12:36 -0300

    Conteúdo do Curso da Udemy

--- Python/Aula-53/Aula - 53.py
+++ Python/Aula-53/Aula - 53.py
@@ -1,3 +1,5 @@
+from random import randint
+
 class Pessoa:
     ano_atual = 2022
 
@@ -9,11 +11,19 @@ class Pessoa:
         print(self.ano_atual - self.idade)
 
     @classmethod
+    # Não utiliza nenhuma instância somente a classe em si
     def por_ano_nascimento(cls, nome, ano_nascimento):
         idade = cls.ano_atual - ano_nascimento
         return cls(nome, idade)
 
+    @staticmethod
+    # Não utiliza nem instância nem a classe em si
+    def gera_id():
+        rand = randint(10000, 19999)
+        return rand
 
 p1 = Pessoa('Gabriel', 19)
-print(p1.nome, p1.idade)
+print(f'Nome: {p1.nome}')
+print(f'Idade: {p1.idade}')
 p1.get_ano_nascimento()
+print(f'Id: {p1.gera_id()}')
