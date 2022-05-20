f818734b4ea9956e15d8becab80b7644d0201f5e
Gabriel <gabrielmachado.emp@gmail.com>
2022-05-20 18:19:03 -0300

    Utilizando Datetime

---
+++ Python/Aula-75/Aula - 75.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from locale import setlocale, LC_ALL
+from calendar import mdays
+
+setlocale(LC_ALL, 'pt_BR')
+
+dt = datetime.now()
+mes_atual = int(dt.strftime('%m'))
+formatacao1 = dt.strftime('%A, %d de %B de %Y')
+formatacao2 = dt.strftime('%d/%m/%Y %H:%M:%S')
+
+print(mes_atual, mdays[mes_atual])
